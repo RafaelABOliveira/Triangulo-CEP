972af5cc5f790dbb0b778aca2b3f9f5943ffad8b
Rafael Oliveira <rafaelntn766@gmail.com>
2025-05-16 20:13:10 -0300

    Ajuste testes

--- TesteCandidatoTDD/TesteTriangulo.cs
+++ TesteCandidatoTDD/TesteTriangulo.cs
@@ -10,26 +10,33 @@ namespace TesteCandidatoTDD
         [TestMethod]
         public void TestResultadoTriangulo()
         {
-            int retorno = new Triangulo().ResultadoTriangulo("[[6],[3,5],[9,7,1],[4,6,8,4]]");
+            int retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[6],[3,5],[9,7,1],[4,6,8,4]]"));
             Assert.IsTrue(retorno == 26);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[6],[3,5],[9,7,1]]");
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[6],[3,5],[9,7,1]]"));
             Assert.IsTrue(retorno == 18);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[6],[3,5]]");
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[6],[3,5]]"));
             Assert.IsTrue(retorno == 11);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[6],[3,5],[9,1,3],[4,6,1,4]]");
-            Assert.IsTrue(retorno == 18);
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[6],[3,5],[9,1,3],[4,6,1,4]]"));
+            Assert.IsTrue(retorno == 24);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[6],[3,5],[9,1,3],[4,6,6,4]]");
-            Assert.IsTrue(retorno == 20);
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[6],[3,5],[9,1,3],[4,6,6,4]]"));
+            Assert.IsTrue(retorno == 24);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[1],[1,1],[1,1,1],[1,1,1,1]]");
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[1],[1,1],[1,1,1],[1,1,1,1]]"));
             Assert.IsTrue(retorno == 4);
 
-            retorno = new Triangulo().ResultadoTriangulo("[[1],[1,1],[1,1,1]]");
+            retorno = new Triangulo().ResultadoTriangulo(ConverterTrianguloJsonParaFormatoSimples("[[1],[1,1],[1,1,1]]"));
             Assert.IsTrue(retorno == 3);
+        }
+
+        private string ConverterTrianguloJsonParaFormatoSimples(string input)
+        {
+            input = input.Replace("[[", "").Replace("]]", "").Replace("],[", ";").Replace("[", "").Replace("]", "").Replace(",", " ");
+
+            return input;
         }
     }
 }
